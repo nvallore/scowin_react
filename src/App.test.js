81fa2e74a85cd99c2dfcf5f5205a805a6066716a
saptis <98163135+saptis@users.noreply.github.com>
2022-07-02 17:11:46 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
@@ -8,3 +8,34 @@ describe("<App />", () => {
     expect(getByText(/Generate Vaccination Reports/i)).toBeInTheDocument();
   });
 });
+
+afterEach(cleanup)
+
+it('should take a snapshot', () => {
+  const { asFragment } = render(<App />)
+  expect(asFragment(<App />)).toMatchSnapshot()
+});
+
+/* 
+jest.mock('axios')
+it("renders correctly enzyme", () => {
+  const wrapper = mount(<App />);
+  expect(toJson(wrapper)).toMatchSnapshot();
+}); 
+describe('routes using memory router', () => {
+  it('should show <Home /> component for "/" route', () => {
+      const component = mount(<MemoryRouter initialEntries={["/"]}><Provider store={store}><App /></Provider></MemoryRouter>);
+      expect(component.find(Home)).toHaveLength(1);
+  });
+  it('should show <Signup /> component for "/signup" route', () => {
+      const component = mount(<MemoryRouter initialEntries={["/signup"]}><App /></MemoryRouter>);
+
+      expect(component.find(Signup)).toHaveLength(1);
+  });
+
+  it('should show <Signin /> component for "/signin" route', () => {
+      const component = mount(<MemoryRouter initialEntries={["/signin"]}><App /></MemoryRouter>);
+
+      expect(component.find(Signin)).toHaveLength(1);
+  });
+});  */
